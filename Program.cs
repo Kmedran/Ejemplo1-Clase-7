90e7239be6ca77b2f61bb4e293588c24c00b90b8
Kmedran <160262184+Kmedran@users.noreply.github.com>
2024-03-16 09:42:02 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo1_Clase_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] calificaciones;
+
+            calificaciones = new int[] { 85, 92, 77, 68, 94 };
+
+            Console.WriteLine(calificaciones[2]);
+        }
+    }
+}
